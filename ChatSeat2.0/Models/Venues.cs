9599854a4c75c97433814e3c0f2128b540ef0311
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-08-19 22:13:43 +0930

    Add Venues controller and model

---
+++ ChatSeat2.0/Models/Venues.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ChatSeat2._0.Models;
+
+public class Venues
+	{
+		public Venues() { }
+		public string Id { get; set; } = default!;
+		public string Name { get; set; } = default!;
+		public string Address { get; set; } = default!;
+		public DateTime? NextSession { get; set; }
+}
+
