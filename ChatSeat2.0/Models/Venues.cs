20c82d7504f75d4b1684f28955b575828ac2d76e
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-08-20 00:31:30 +0930

    Update package.json about MVC_PORT number

--- ChatSeat2.0/Models/Venues.cs
+++ ChatSeat2.0/Models/Venues.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 namespace ChatSeat2._0.Models;
 
+public class Slots {
+	public string Date { get; set; } = default!;
+    public string Time { get; set; } = default!;
+}
 public class Venues
 	{
 		public Venues() { }
 		public string Id { get; set; } = default!;
 		public string Name { get; set; } = default!;
-		public string Address { get; set; } = default!;
-		public DateTime? NextSession { get; set; }
+		public string Location { get; set; } = default!;// May use it for map
+		public List<Slots> UpcomingSlots { get; set; } = new();// Holds all available future time slots for this venue.
+    //public DateTime? NextSlot { get; set; }// Shows the next upcoming slot at this venue
+		
 }
 
