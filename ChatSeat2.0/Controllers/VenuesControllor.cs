3b0ea196aa6ec7c6ab9eb8b3a795da1c48d7251e
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-08-19 22:41:44 +0930

    Add VenuesController returns one venue with available slots.

---
+++ ChatSeat2.0/Controllers/VenuesControllor.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using ChatSeat2._0.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSeat2._0.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class VenuesControllor : ControllerBase
+	{
+		[HttpGet]
+        public IActionResult GetVenues()
+        {
+            var venue = new Venues
+            {
+                Name = "Campbelltown Library",
+                Address = "171 Montacute Rd, Newton SA 5074",
+                UpcomingSlots = new List<Slots>
+                {
+                    new Slots { Date = "Monday", Time = "10:00" },
+                    new Slots { Date = "Monday", Time = "11:00" },
+                    new Slots { Date = "Tuesday", Time = "10:00" },
+                    new Slots { Date = "Wednesday", Time = "10:00" },
+                    new Slots { Date = "Thursday", Time = "10:00" }
+                }
+            };
+
+            return Ok(venue);
+        }
+    }
+}
+}
+
