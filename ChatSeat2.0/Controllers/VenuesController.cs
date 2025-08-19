e09ab620622868881b7d207deb32e2ea6ab06d47
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-08-20 00:21:36 +0930

    Update Route

---
+++ ChatSeat2.0/Controllers/VenuesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using ChatSeat2._0.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatSeat2._0.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class VenuesController : ControllerBase
+	{
+		[HttpGet]
+        public IActionResult GetVenues()
+        {
+            var venue = new Venues
+            {
+                Id = "venue1",
+                Name = "Campbelltown Library",
+                Location = "171 Montacute Rd, Newton SA 5074",
+                UpcomingSlots = new List<Slots>
+                {
+                    new Slots { Date = "Monday", Time = "10:00"},
+                    new Slots { Date = "Monday", Time = "11:00" },
+                    new Slots { Date = "Tuesday", Time = "10:00" },
+                    new Slots { Date = "Wednesday", Time = "10:00" },
+                    new Slots { Date = "Thursday", Time = "10:00" }
+                }
+            };
+
+            return Ok(venue);
+        }
+    }
+}
+
+
--- ChatSeat2.0/csapp/src/App.js
+++ ChatSeat2.0/csapp/src/App.js
@@ -4,22 +4,8 @@ import { ToastContainer } from "react-toastify";
 import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Login from "./components/Login";
-//import Venues from "./components/Venues";
+import Venues from "./components/Venues";
 
-/*Placeholder, need one teammate done (I think TO Jordon ^^ cause Callum will do supabase stuff)
-if you want to achieve this feature,
-please create a branch name feature-Venues, then start work on it,
-Please dont update App.js or index.js file, in case too many conflit
-( when you push please make sure this App.js keep like this)
-*/
-function Venues() {
-    return (
-        <div className="container py-5">
-            <h2 className="mb-3">Available Venues</h2>
-            <p>Update later....</p>
-        </div>
-    );
-}
 
 export default function App() {
     return (
