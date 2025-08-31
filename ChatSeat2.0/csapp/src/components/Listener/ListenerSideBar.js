de18014e1d953dc44bed9ca3b8cd6f391abda028
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-09-01 00:51:02 +0930

    Migrate Listener Dashboard Content

---
+++ ChatSeat2.0/csapp/src/components/Listener/ListenerSideBar.js
@@ -0,0 +1,23 @@
+﻿import { NavLink } from "react-router-dom";
+
+export default function ListenerSidebar() {
+    const linkCls = ({ isActive }) =>
+        "list-group-item list-group-item-action rounded-pill mb-2 text-center " +
+        (isActive ? "active" : "");
+
+    return (
+        <aside className="p-3" style={{ width: 260, background: "#A8E4F2" }}>
+            <div className="list-group border-0">
+                <NavLink to="/coordinatorslistinlistener" className={linkCls}>
+                    List of Coordinators
+                </NavLink>
+                <NavLink to="/listenerscheduling" className={linkCls}>
+                    Scheduling
+                </NavLink>
+                <NavLink to="/listenerchatroom" className={linkCls}>
+                    Let’s Talk
+                </NavLink>
+            </div>
+        </aside>
+    );
+}
