165efa733a9fa4038c926409ac798dfc4b0c1764
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-10-02 03:03:14 +0930

    Keep same logout sequence at the listener dashboard

--- ChatSeat2.0/csapp/src/components/Listener/ListenerSideBar.js
+++ ChatSeat2.0/csapp/src/components/Listener/ListenerSideBar.js
@@ -1,18 +1,31 @@
 ﻿import { NavLink, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../state/loggedInUser";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 export default function ListenerSidebar() {
     const location = useLocation();
     const navigate = useNavigate();
+    const dispatch = useDispatch(); 
+
 
     const getActiveLink = (url) =>
         location.pathname === url
             ? "admin-sidebar__link active"
             : "admin-sidebar__link";
 
-    const handleLogout = () => {
-        // Add logout logic here
-        //navigate("/login");
-        navigate("/");
+    const handleLogout = async () => {
+        // Supabase logout
+        await supabase.auth.signOut();
+
+        // Clear Redux state
+        dispatch(logoutUser());
+        navigate("/");                          
     };
 
     return (
