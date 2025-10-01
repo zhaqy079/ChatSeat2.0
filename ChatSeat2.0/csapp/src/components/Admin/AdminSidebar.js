dfe0ddfcd0fee8003098d47298817ff90a434c01
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-10-02 02:58:19 +0930

    Implement logout feature return to the home page

--- ChatSeat2.0/csapp/src/components/Admin/AdminSidebar.js
+++ ChatSeat2.0/csapp/src/components/Admin/AdminSidebar.js
@@ -1,20 +1,30 @@
 import { Link, useLocation, useNavigate } from "react-router-dom";
-import '../../index.css';
+import { useDispatch } from "react-redux";
+import { logoutUser } from "../../state/loggedInUser";
+import { createClient } from "@supabase/supabase-js";
 
-
+const supabase = createClient(
+    process.env.REACT_APP_SUPABASE_URL,
+    process.env.REACT_APP_SUPABASE_ANON_KEY
+);
 
 export default function AdminSidebar({ userName = "" }) {
     const location = useLocation();
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const getActiveLink = (url) =>
         location.pathname === url
             ? "admin-sidebar__link active"
             : "admin-sidebar__link";
 
-    const handleLogout = () => {
-        // Add logout logic here
-        //navigate("/login");
+    const handleLogout = async () => {
+ 
+        // Supabase logout
+        await supabase.auth.signOut();
+
+        // Clear Redux state
+        dispatch(logoutUser());
         navigate("/");
     };
 
