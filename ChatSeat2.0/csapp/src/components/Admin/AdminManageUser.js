41c4e22799c45770ba9c4938321fa90bfc375a31
malcw001 <malcw001@mymail.unisa.edu.au>
2025-10-17 20:34:50 +1030

    Updating privileges successfully working :)

--- ChatSeat2.0/csapp/src/components/Admin/AdminManageUser.js
+++ ChatSeat2.0/csapp/src/components/Admin/AdminManageUser.js
@@ -43,15 +43,13 @@ const deleteUser = async (userID, navigate) => {
     const confirmed = window.confirm("Are you sure you want to delete this user? \n There is no way to undo this action. ");
 
     if (confirmed) {
-        const { profError } = await supabase.auth.admin.deleteUser(userID)
-        const authError = await supabase
+        const profError = await supabase
             .from('user_profiles')
             .delete()
             .eq('profile_id', userID)
 
-        if (profError || authError) {
+        if (profError) {
             console.log("User profile response: " + profError);
-            console.log("User auth response: " + authError);
             alert("Failed to completely delete user.");
             return;
         }
@@ -59,17 +57,14 @@ const deleteUser = async (userID, navigate) => {
 
         navigate("/adminViewUsers");
     } else {
-        // Cancelled
         console.log("Action canceled.");
     }
 
 };
 
-// Logic to make/remove coordinator and admin privileges
-const updatePrivileges = (e) => {
-    e.preventDefault();
-    console.log("In da loop!!!");
-};
+
+
+
 
 
 export default function AdminManageUser() {
@@ -77,6 +72,95 @@ export default function AdminManageUser() {
     const [user, setUser] = useState([]);
     const [locationlist, setLocationlist] = useState([]);
     const { id } = useParams();
+
+    // Logic to make/remove coordinator and admin privileges
+    const updatePrivileges = (event) => {
+        event.preventDefault();
+        const formData = new FormData(event.target);
+        const isAdmin = formData.get('admin') === 'on';
+        const updateLocationIds = locationlist
+            .filter(location => formData.get(location.location_id.toString()) === 'on')
+            .map(location => location.location_id);
+
+
+        if (isAdmin === (user.admin_profiles === null)) {
+            console.log("In admin loop!");
+
+            updateAdmin(isAdmin)
+        }
+
+        locationlist.map((location) => {
+            console.log(location.location_id)
+            const diff = user.coordinator_profiles.some(coord_location => coord_location.location_id === location.location_id);
+
+            if (updateLocationIds.includes(location.location_id)) {
+                console.log("Has been checked.")
+                if (!diff) {
+                    console.log("Difference to user data!");
+
+                    updateCoord(true, location.location_id)
+                }
+            } else {
+                console.log("Unchecked.")
+                if (diff) {
+                    console.log("Difference to user data!");
+
+                    updateCoord(false, location.location_id)
+                }
+            }
+        })
+    };
+
+    const updateAdmin = async (state) => {
+        var adminError = '';
+        if (state === true) {
+            adminError = await supabase
+                .from('admin_profiles')
+                .insert({
+                    admin_id: user.profile_id,
+                    approved_by: "73fd19d1-5665-479b-8500-5ea691b0e1be"
+                })
+
+            console.log("User admin added.");
+        } else {
+            adminError = await supabase
+                .from('admin_profiles')
+                .delete()
+                .eq('admin_id', user.profile_id)
+
+            console.log("User admin removed.");
+        }
+
+        if (adminError) {
+            console.log("User admin response: " + adminError);
+        }
+    }
+
+    const updateCoord = async (state, locationID) => {
+        var coordError = '';
+        if (state === true) {
+            coordError = await supabase
+                .from('coordinator_profiles')
+                .insert({
+                    coordinator_id: user.profile_id,
+                    location_id: locationID,
+                    approved_by: "73fd19d1-5665-479b-8500-5ea691b0e1be"
+                })
+
+            console.log("User admin added to location: " + locationID);
+        } else {
+            coordError = await supabase
+                .from('coordinator_profiles')
+                .delete()
+                .eq('location_id', locationID, 'coordinator_id', user.profile_id)
+
+            console.log("User coord removed for location: " + locationID);
+        }
+
+        if (coordError) {
+            console.log("User coord response: " + coordError + ", for location: " + locationID);
+        }
+    }
 
 
     // Stores the list of users from the database
@@ -149,7 +233,7 @@ export default function AdminManageUser() {
                                                 </td>
                                                 <td className="text-center">
                                                     <div className="justify-content-center">
-                                                        <input type="checkbox" id={location.location_id} defaultChecked={(user.coordinator_profiles.some(coord_location => coord_location.location_id === location.location_id))} />
+                                                        <input type="checkbox" name={location.location_id} defaultChecked={(user.coordinator_profiles.some(coord_location => coord_location.location_id === location.location_id))} />
                                                     </div>
                                                 </td>
                                             </tr>
@@ -160,7 +244,7 @@ export default function AdminManageUser() {
                             <hr />
                             <h5>Admin Privileges</h5>
                             <label>
-                                Admin: <input type="checkbox" id="admin" defaultChecked={!(user.admin_profiles === null)} />
+                                Admin: <input type="checkbox" name="admin" defaultChecked={!(user.admin_profiles === null)} />
                             </label>
                             <hr/>
                             <div className="d-flex align-items-center">
