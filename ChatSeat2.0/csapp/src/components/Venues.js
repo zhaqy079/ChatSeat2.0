f99120ae72f048f6dce6e6cdd5332c54898ad20a
Qianting <zhaqy079@mymail.unisa.edu.au>
2025-11-12 21:50:55 +1030

    Fix the homepage pic fix width, venue page typo

--- ChatSeat2.0/csapp/src/components/Venues.js
+++ ChatSeat2.0/csapp/src/components/Venues.js
@@ -203,7 +203,7 @@ export default function Venues() {
                                                                 <strong> <img src={listenerIcon} alt="" className="icon" style={{ width: 24, height: 24 }} aria-hidden="true" />
                                                                     Listeners: </strong>
                                                             </div>
-                                                         $
+
                                                             <ul className="list-unstyled ms-3 mt-1 mb-0">
                                                                 {(appointment.bookedUsers && appointment.bookedUsers.length > 0
                                                                     ? appointment.bookedUsers
@@ -226,7 +226,7 @@ export default function Venues() {
                         })
                     )}
                 </div>
-                    
+
             </div>
         </div>
     );
